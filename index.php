699d7bb657483f2acf4b3f82105e3a2dbc8c8bf0
juanpabloruiz <pabloruiz1980@gmail.com>
2023-03-20 08:55:22 -0300

    Cambios en estructura

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php
-session_start();
-$conexion = new mysqli('localhost', 'usuario', '123', 'base');
-mysqli_set_charset($conexion, ('utf8'));
-?>
+<?php include('conexion.php'); ?>
 <!DOCTYPE html>
 <html lang="es">
 
