ba441692925bd26388c761e66b72a4fe9b9aaca1
juanpabloruiz <pabloruiz1980@gmail.com>
2023-03-20 08:48:07 -0300

    Subiendo más archivos

--- conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$conexion = new mysqli('localhost', 'usuario', '123', 'base');
-mysqli_set_charset($conexion, 'utf8');
-?>
--- index.php
+++ index.php
@@ -1,3 +1,64 @@
-<?php include('cabecera.php'); ?>
-<h1>Hola</h1>
-<?php include('pie.php'); ?>
+<?php
+session_start();
+$conexion = new mysqli('localhost', 'usuario', '123', 'base');
+mysqli_set_charset($conexion, ('utf8'));
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi web</title>
+    <meta name="description" content="Sitio web de mis cosas personales con buen contenido.">
+    <meta name="keywords" content="Blog, contenido, interesante, historias, archivos">
+    <meta name="Author" content="Usuario">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./">Mi web</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="p-5">
+        <a href="./">
+            <img src="img/logo.png" alt="Logotipo" class="img-fluid">
+        </a>
+        <h2>Mi blog personal</h2>
+    </header>
+    <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
